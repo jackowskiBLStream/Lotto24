3033e026e278c8ebab7a2f3c0ef96b0a86267a7a
jackowskiBLStream <damian.jackowski@blstream.com>
2016-03-15 14:22:08 +0100

    Alpha version without tests

--- app/src/main/java/com/blstream/lotto24/MainActivity.java
+++ app/src/main/java/com/blstream/lotto24/MainActivity.java
@@ -3,6 +3,8 @@ package com.blstream.lotto24;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -12,15 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        // setContentView(R.layout.activity_main);
-        PixelGridView pixelGrid = null;
+       /* PixelGridView pixelGrid = null;
         try {
-            pixelGrid = new PixelGridView(this, 10, 10);
+            pixelGrid = new PixelGridView(this, 7, 7);
         } catch (IOException e) {
             e.printStackTrace();
         }
         pixelGrid.setSaveEnabled(true);
-        pixelGrid.setId(R.id.chessBoardRelativeLayout);
-        setContentView(pixelGrid);
+        pixelGrid.setId(R.id.chessBoardRelativeLayout);*/
+        setContentView(R.layout.custom_layout);
+
+        PixelGridView pixel = (PixelGridView) findViewById(R.id.pixel);
+
+        pixel.setOnSwipeListener(new PixelGridView.OnSwipeListener() {
+            @Override
+            public void onSwipeRight() {
+                Toast.makeText(getApplicationContext(), "Right swipe detected", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                Toast.makeText(getApplicationContext(), "Left swipe detected", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
--- app/src/main/java/com/blstream/lotto24/PixelGridView.java
+++ app/src/main/java/com/blstream/lotto24/PixelGridView.java
@@ -2,14 +2,13 @@ package com.blstream.lotto24;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.Typeface;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.text.TextPaint;
@@ -22,11 +21,12 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * Created by blstream on 3/14/2016.
+ *
  */
 public class PixelGridView extends View {
 
 
+    float x1, x2, y1, y2;
     private int numColumns, numRows;
     private int cellWidth, cellHeight;
     private TextPaint textPaint = new TextPaint();
@@ -36,20 +36,34 @@ public class PixelGridView extends View {
     private int checkedCounter;
     private boolean[][] cellChecked = null;
     private int height, width;
-
     private boolean isSaved = false;
+    private OnSwipeListener onSwipeListener;
 
-
-    public PixelGridView(Context context, int numColumns, int numRows) throws IOException {
+   /* public PixelGridView(Context context, int numColumns, int numRows) throws IOException {
         this(context, null, numColumns, numRows);
 
-    }
+    }*/
 
-    public PixelGridView(Context context, AttributeSet attrs, int numColumns, int numRows) throws IOException {
+    public PixelGridView(Context context, AttributeSet attrs) throws IOException {
         super(context, attrs);
+        TypedArray attributeValuesArray = context.getTheme()
+                .obtainStyledAttributes(attrs, R.styleable.PixelGridView, 0, 0);
+
+        String numColumns = attributeValuesArray.getString(R.styleable.PixelGridView_numColums);
+        String numRows = attributeValuesArray.getString(R.styleable.PixelGridView_numRows);
+        if(numColumns != null){
+            this.numColumns = Integer.parseInt(numColumns);
+        }else{
+            this.numColumns = 1;
+        }
+        if(numRows != null){
+            this.numRows = Integer.parseInt(numRows);
+        } else{
+            this.numRows = 1;
+        }
+
+        onSwipeListener = null;
         //Init only once(Manifest android:configChanges)
-        this.numColumns = numColumns;
-        this.numRows = numRows;
         textPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         greenPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         greenPaint.setColor(Color.GREEN);
@@ -58,9 +72,13 @@ public class PixelGridView extends View {
         borderPaint.setStrokeWidth(20);
         setSaveEnabled(true);
 
-
-
     }
+
+
+    public void setOnSwipeListener(OnSwipeListener onSwipeListener) {
+        this.onSwipeListener = onSwipeListener;
+    }
+
     public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
         int width = bm.getWidth();
         int height = bm.getHeight();
@@ -125,7 +143,7 @@ public class PixelGridView extends View {
             cellWidth = getWidth() / numColumns;
             cellHeight = cellWidth;
         }
-//TODO: init only once!
+        //init only once
         if (cellChecked == null) {
             cellChecked = new boolean[numColumns][numRows];
         }
@@ -156,34 +174,29 @@ public class PixelGridView extends View {
         drawChessBoard(borderPaint, canvas, height, width);
         Log.d("No. counter: ", String.valueOf(checkedCounter));
 
-        //drawBorder(borderPaint, canvas, height, width);
 
 
     }
 
     private void drawCheckMarks(Canvas canvas) {
-        //Drawing checkMark
         for (int i = 0; i < numColumns; i++) {
             for (int j = 0; j < numRows; j++) {
                 if (cellChecked[i][j]) {
                     canvas.drawBitmap(bitmap, i * cellWidth, j * cellHeight,
                             textPaint);
-
                 }
             }
         }
     }
 
     public void drawNumbers(Canvas canvas) {
-        //drawNumbers
         textPaint.setTextSize(cellHeight * 0.5f);
-        //TODO: text should takes 50% of height
         for (int i = 0; i < numColumns; i++) {
             for (int j = 0; j < numRows; j++) {
-                    canvas.drawText(String.valueOf((i * numRows + j + 1)), (j + 1)
-                            * cellWidth - cellWidth / 2 - textPaint.measureText
-                            (String.valueOf((i * numRows + j + 1))) / 2, (i + 1) * cellHeight -
-                            cellHeight / 2 - (textPaint.descent() + textPaint.ascent()) / 2, textPaint);
+                canvas.drawText(String.valueOf((i * numRows + j + 1)), (j + 1)
+                        * cellWidth - cellWidth / 2 - textPaint.measureText
+                        (String.valueOf((i * numRows + j + 1))) / 2, (i + 1) * cellHeight -
+                        cellHeight / 2 - (textPaint.descent() + textPaint.ascent()) / 2, textPaint);
             }
         }
 
@@ -213,38 +226,53 @@ public class PixelGridView extends View {
         canvas.drawRect(0, 0, width, height, paint);
     }
 
-    public void drawBorder(Paint paint, Canvas canvas, int height, int width) {
-/*        if (checkedCounter == 6) {
-            paint.setColor(Color.GREEN);
-        } else {
-            paint.setColor(Color.YELLOW);
-        }*/
-        canvas.drawRect(0, 0, width, height, paint);
-    }
-
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN && event.getX() < width - 5 && event.getY() < height - 5) {
-            int column = (int) (event.getX() / cellWidth);
-            int row = (int) (event.getY() / cellHeight);
+        if (event.getX() < width - 5 && event.getY() < height - 5) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                int column = (int) (event.getX() / cellWidth);
+                int row = (int) (event.getY() / cellHeight);
 
 
-            if (checkedCounter == 6 && !cellChecked[column][row]) {
+                if (checkedCounter == 6 && !cellChecked[column][row]) {
 
-            } else if (cellChecked[column][row]) {
-                cellChecked[column][row] = !cellChecked[column][row];
-                checkedCounter--;
-            } else {
-                cellChecked[column][row] = !cellChecked[column][row];
-                checkedCounter++;
+                } else if (cellChecked[column][row]) {
+                    cellChecked[column][row] = !cellChecked[column][row];
+                    checkedCounter--;
+                } else {
+                    cellChecked[column][row] = !cellChecked[column][row];
+                    checkedCounter++;
+                }
+                invalidate();
             }
-
-
-            // Log.d("No. counter: ", String.valueOf(checkedCounter));
-            invalidate();
+            detectSwipe(event);
         }
 
+
         return true;
+    }
+
+    public void detectSwipe(MotionEvent event) {
+
+        switch (event.getAction()) {
+            // when user first touches the screen we get x and y coordinate
+            case MotionEvent.ACTION_DOWN: {
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                x2 = event.getX();
+                y2 = event.getY();
+                if (x1 < x2) {
+                    onSwipeListener.onSwipeRight();
+
+                } else if (x1 > x2) {
+                    onSwipeListener.onSwipeLeft();
+                }
+                break;
+            }
+        }
     }
 
     @Override
@@ -262,9 +290,15 @@ public class PixelGridView extends View {
     public void onRestoreInstanceState(Parcelable state) {
         SavedState ss = (SavedState) state;
         super.onRestoreInstanceState(ss.getSuperState());
-        // currentValue = ss.value;
         cellChecked = ss.mArray;
         checkedCounter = ss.mCheckCounter;
+    }
+
+    public interface OnSwipeListener {
+        void onSwipeRight();
+
+        void onSwipeLeft();
+
     }
 
     private static class SavedState extends BaseSavedState {
@@ -288,7 +322,6 @@ public class PixelGridView extends View {
         private SavedState(Parcel in) {
             super(in);
             boolean[][] array;
-            //array = in.readArray(ClassLoader.getSystemClassLoader())
             final int N = in.readInt();
             array = new boolean[N][N];
             for (int i = 0; i < N; i++) {
@@ -296,7 +329,6 @@ public class PixelGridView extends View {
             }
             mArray = array;
             mCheckCounter = in.readInt();
-            // array = in.readBooleanArray();
         }
 
         @Override
@@ -309,8 +341,6 @@ public class PixelGridView extends View {
                 out.writeBooleanArray(mArray[i]);
             }
             out.writeInt(mCheckCounter);
-
-            // out.writeTypedArray(array);
         }
     }
 }
